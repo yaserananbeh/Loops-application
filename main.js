834a72a51ff6d2d5864c3802dac04bb248a140f8
Yaser Al-Ananbeh <yaser.ananbeh74@gmail.com>
2021-03-07 18:00:24 +0200

    fix some problems

--- main.js
+++ main.js
@@ -11,16 +11,16 @@ var iterationNo= prompt("In this field any number you want to show the Number th
 if( firstEnter=="1"){
     var Item='<img class="mainImg" src="https://t4.ftcdn.net/jpg/03/09/02/71/240_F_309027197_EXxztQT5RfTMMMRCqwQHp1PXEhaYHMjM.jpg" alt="Number One">'
 }
-else if (firstEnter="2"){
+else if (firstEnter=="2"){
     var Item='<img class="mainImg" src="https://t4.ftcdn.net/jpg/03/09/02/71/240_F_309027194_96P1Uo3rM25gqMgd53ZXmnywVSw4wlgw.jpg" alt="Number Two">'
 }
-else if (firstEnter="3"){
+else if (firstEnter=="3"){
     var Item='<img class="mainImg" src="https://t4.ftcdn.net/jpg/03/09/02/71/240_F_309027152_XvUOqEQMW8AuaPcgdIXwN2tPv1tzhsL4.jpg" alt="Number Three">'
 }
-else if (firstEnter="4"){
+else if (firstEnter=="4"){
     var Item='<img class="mainImg" src="https://t4.ftcdn.net/jpg/03/09/02/71/240_F_309027137_n3PfvH0a4hTENcyQSJWkd9TYYTcm5Toi.jpg" alt="Number Four">'
 }
-else if (firstEnter="5"){
+else if (firstEnter=="5"){
     var Item='<img class="mainImg" src="https://t3.ftcdn.net/jpg/03/09/02/70/240_F_309027088_71DiXcE40DteR7QRmc9fFmiaunPkE6hC.jpg" alt="Number Five">'
 }
 for(i=0;i<iterationNo;i++){
